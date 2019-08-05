12c2fb37981c0418d47bd9f05b99fa267bd0bad5
Abhilash Palem <abhilashpalem@mac-pro-abhilash-palem.local>
2019-08-05 15:26:39 +0530

    Initial Commit

---
+++ CBVogoManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CBVogoManager
+//
+//  Created by Abhilash Palem on 05/08/19.
+//  Copyright © 2019 Abhilash Palem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
